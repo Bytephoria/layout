4322686de91d95b94733bda9fce155f1fe66401f
iAmForyyDev <iAmForyyDev@gmail.com>
2025-09-12 07:08:55 -0500

    style(item): compact InventoryClickContext record declaration

--- item/src/main/java/team/bytephoria/layout/items/context/InventoryClickContext.java
+++ item/src/main/java/team/bytephoria/layout/items/context/InventoryClickContext.java
@@ -3,7 +3,4 @@ package team.bytephoria.layout.items.context;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
-public record InventoryClickContext(
-        Player player,
-        InventoryClickEvent clickEvent
-) {}
+public record InventoryClickContext(Player player, InventoryClickEvent clickEvent) {}
