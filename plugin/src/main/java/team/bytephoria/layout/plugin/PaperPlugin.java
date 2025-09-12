618ba07ad9fce2498bab27dd99788a5f47e3f130
iAmForyyDev <iAmForyyDev@gmail.com>
2025-09-11 19:46:38 -0500

    refactor(plugin): remove example command registration and clean up PaperPlugin
    Removed automatic registration of example commands
    (`FixedLayoutCommand`, `SizedLayoutCommand`, `SizedPatternLayoutCommand`)
    and deleted the `registerCommand` method to avoid side effects when
    using this module as a dependency.
    
    Also added a clarifying comment to `Layout.init(this)` indicating it
    is required for the library to function properly.

--- plugin/src/main/java/team/bytephoria/layout/plugin/PaperPlugin.java
+++ plugin/src/main/java/team/bytephoria/layout/plugin/PaperPlugin.java
@@ -1,29 +1,13 @@
 package team.bytephoria.layout.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 import team.bytephoria.layout.layouts.Layout;
-import team.bytephoria.layout.plugin.commands.abstraction.AbstractBukkitCommand;
-import team.bytephoria.layout.plugin.commands.FixedLayoutCommand;
-import team.bytephoria.layout.plugin.commands.SizedLayoutCommand;
-import team.bytephoria.layout.plugin.commands.SizedPatternLayoutCommand;
 
 public final class PaperPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        // Initialize the Layout library (required for the library to work properly)
         Layout.init(this);
-
-        this.registerCommand(
-                new FixedLayoutCommand(),
-                new SizedLayoutCommand(),
-                new SizedPatternLayoutCommand()
-        );
-    }
-
-    private void registerCommand(final @NotNull AbstractBukkitCommand @NotNull ... abstractBukkitCommand) {
-        for (final AbstractBukkitCommand abstractBukkitCommand1 : abstractBukkitCommand) {
-            this.getServer().getCommandMap().register("layout", abstractBukkitCommand1);
-        }
     }
 }
