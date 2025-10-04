b9cb0f334faf229bc79653acf4325264dfe034c7
iAmForyyDev <iAmForyyDev@gmail.com>
2025-10-03 20:26:25 -0500

    refactor(item): improve method naming and add Javadoc to SkullItemBuilder

--- item/src/main/java/team/bytephoria/layout/items/base/builder/SkullItemBuilder.java
+++ item/src/main/java/team/bytephoria/layout/items/base/builder/SkullItemBuilder.java
@@ -21,29 +21,65 @@ public class SkullItemBuilder extends ItemBuilder<SkullItemBuilder, SkullItem> {
         return this;
     }
 
-    public SkullItemBuilder playerProfile(final @NotNull PlayerProfile playerProfile) {
+    /**
+     * Sets a custom {@link PlayerProfile} for this skull.
+     *
+     * @param playerProfile the player profile to use
+     * @return this builder
+     */
+    public SkullItemBuilder withPlayerProfile(final @NotNull PlayerProfile playerProfile) {
         this.playerProfile = playerProfile;
         return this;
     }
 
-    public SkullItemBuilder player(final @NotNull Player player) {
+    /**
+     * Sets the skull texture using the profile of a specific player.
+     *
+     * @param player the player whose profile will be used
+     * @return this builder
+     */
+    public SkullItemBuilder fromPlayer(final @NotNull Player player) {
         this.playerProfile = player.getPlayerProfile();
         return this;
     }
 
-    public SkullItemBuilder texture(final @NotNull String texture) {
+    /**
+     * Sets the skull texture using a Base64-encoded texture value.
+     * <p>
+     * Example:
+     * <pre>{@code
+     * builder.withTextureValue("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOTc4ZDNjYmE0MWRj...");
+     * }</pre>
+     *
+     * @param base64Texture the Base64 texture value (the full VALUE property)
+     * @return this builder
+     */
+    public SkullItemBuilder withTextureValue(final @NotNull String base64Texture) {
         this.playerProfile = Bukkit.createProfile(UUID.randomUUID());
-        this.playerProfile.setProperty(new ProfileProperty("textures", texture));
+        this.playerProfile.setProperty(new ProfileProperty("textures", base64Texture));
         return this;
     }
 
-    public SkullItemBuilder skin(final @NotNull String skin) {
+    /**
+     * Sets the skull skin using a Mojang texture ID.
+     * <p>
+     * This ID corresponds to the last part of the Mojang texture URL:
+     * <pre><a href="http://textures.minecraft.net/texture/&lt;skinId&gt">https://textures.minecraft.net/texture/...</a>;</pre>
+     * Example:
+     * <pre>{@code
+     * builder.withSkinId("978d3cba41dc11eba418d8c67d64db6cb179bc6abfce7ad0703fb4d7369d689a");
+     * }</pre>
+     *
+     * @param skinId the texture ID from Mojang’s texture server
+     * @return this builder
+     */
+    public SkullItemBuilder withSkinId(final @NotNull String skinId) {
         this.playerProfile = Bukkit.createProfile(UUID.randomUUID());
         final PlayerTextures playerTextures = this.playerProfile.getTextures();
         final String url = "http://textures.minecraft.net/texture/";
 
         try {
-            playerTextures.setSkin(URI.create(url + skin).toURL());
+            playerTextures.setSkin(URI.create(url + skinId).toURL());
             this.playerProfile.setTextures(playerTextures);
         } catch (MalformedURLException ignored) {
         }
@@ -61,5 +97,4 @@ public class SkullItemBuilder extends ItemBuilder<SkullItemBuilder, SkullItem> {
                 this.playerProfile
         );
     }
-
 }
